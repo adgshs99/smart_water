7ef06a94d9b1993cdbc3326c5ba0cd44002799bc
adgshs99 <anasdgsh@hotmail.com>
2025-01-07 22:53:35 +0200

    Added Arduino project k12_WifiClient

--- k12_WifiClient/SmartWater.ino
+++ k12_WifiClient/SmartWater.ino
@@ -1,12 +1,44 @@
-void setup()
-{
-  Serial.begin(115200);
-  WiFi_SETUP();
-}
+// void setup()
+// {
+//   Serial.begin(115200);
+//   WiFi_SETUP();
+// }
 
 
-void loop()
-{
-  sendData();
-  delay(10000);
-}
+// void loop()
+// {
+//   sendData();
+//   delay(10000);
+// }
+// #define A1A 19
+// #define A1B 18
+// void setup() {
+// pinMode(A1A,OUTPUT);
+// pinMode(A1B,OUTPUT);
+
+// digitalWrite(A1A, LOW);
+// digitalWrite(A1B, HIGH);
+// Serial.println("bumb has started");
+// delay(10000);
+// digitalWrite(A1A, HIGH);
+// digitalWrite(A1B, HIGH);
+// Serial.println("bumb has stopped");
+
+
+// }
+
+
+// void loop() {
+	
+// }
+    #define DHTTYPE DHT11
+    void setup()
+    {
+    DHT dht(DHTPIN, DHTTYPE); 
+	  dht.begin(); 
+
+    } 
+    void loop(){
+          float t = dht.readTemperature(); 
+
+    }  
